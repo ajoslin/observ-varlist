6c8a3842a45e487c4e264b57fda5d830fb3fd47b
Andrew Joslin <andrewtjoslin@gmail.com>
2016-09-10 06:58:36 -0400

    listToArray: make it slightly more efficient

--- to-array.js
+++ to-array.js
@@ -3,12 +3,13 @@ var ObservArray = require('observ-array')
 module.exports = function toArray (list) {
   var isObserv = typeof list === 'function'
 
-  var array = []
   var from = isObserv ? list.from() : list.from
   var count = isObserv ? list.count() : list.count
+  var array = new Array(from + count)
+  var arrayIndex = 0
 
   for (var index = from, length = from + count; index < length; index++) {
-    array.push(list[index])
+    array[arrayIndex++] = list[index]
   }
 
   return isObserv ? ObservArray(array) : array
